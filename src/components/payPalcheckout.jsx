387e389b35b126c173c2dbf1acc58a0f623f3410
V. Puentes <68936403+V-Puentes@users.noreply.github.com>
2025-04-01 01:24:44 -0300

    avance paypal

--- src/components/payPalcheckout.jsx
+++ src/components/payPalcheckout.jsx
@@ -4,25 +4,24 @@ import axios from "axios";
 function PayPalCheckout() {
     return (
         <PayPalScriptProvider options={{ "client-id": "AXSS4dtnRC6boUJeK3tcL_ijaw43cDA6a42JzTxziY4zbFRUPI_rymC8j5nxJiI3CvpbgKl63aIQbYWH" }}>
-  <div style={{ width: "100%", textAlign: "center", padding: "20px" }}>
-    <PayPalButtons
-      createOrder={async () => {
-        const { data } = await axios.post("http://localhost:3001/create-order");
-        return data.id;
-      }}
-      onApprove={async (data) => {
-        const response = await axios.post("http://localhost:3001/capture-order", {
-          orderID: data.orderID,
-        });
-        console.log("Pago completado:", response.data);
-      }}
-    />
-  </div>
-</PayPalScriptProvider>
-
+            <div style={{ width: "100%", textAlign: "center", padding: "20px" }}>
+                <PayPalButtons
+                    createOrder={async () => {
+                        // Solicitud al backend en Vercel
+                        const { data } = await axios.post("https://pocketcenter-backend.vercel.app/create-order");
+                        return data.id;
+                    }}
+                    onApprove={async (data) => {
+                        // Solicitud para capturar el pago
+                        const response = await axios.post("https://pocketcenter-backend.vercel.app/capture-order", {
+                            orderID: data.orderID,
+                        });
+                        console.log("Pago completado:", response.data);
+                    }}
+                />
+            </div>
+        </PayPalScriptProvider>
     );
 }
 
 export default PayPalCheckout;
-
-
